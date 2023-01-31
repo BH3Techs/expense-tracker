f7703c472ae366972e7ae6fdf36d1b518dd2294b
Howard Mabhugu <hmabhugu.3@gmail.com>
2023-01-31 09:48:36 +0200

    Added Expense Item css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,8 @@
+import './ExpenseItem.css';
+
 function ExpenseItem(){
     return (
-        <div>
+        <div className='expense-item'>
             <h1>Expense Item 1</h1>
             <h1>Expense Item Date</h1>
             <h1>Expense Item Price</h1>
