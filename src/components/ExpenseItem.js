6801d294b613e6cf73855b414a9603746479809f
Howard Mabhugu <hmabhugu.3@gmail.com>
2023-01-31 09:46:02 +0200

    My First component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <h1>Expense Item 1</h1>
+            <h1>Expense Item Date</h1>
+            <h1>Expense Item Price</h1>
+        </div>
+    );
+}
+
+export default ExpenseItem;
