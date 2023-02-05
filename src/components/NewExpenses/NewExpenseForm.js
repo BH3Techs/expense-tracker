adfc7fb2e31e6c805eafa6b8d883f604280d8820
Howard Mabhugu <hmabhugu.3@gmail.com>
2023-02-05 18:22:16 +0200

    used used state

--- src/components/NewExpenses/NewExpenseForm.js
+++ src/components/NewExpenses/NewExpenseForm.js
@@ -1,20 +1,34 @@
+import React, {useState} from 'react';
 import './NewExpenseForm.css';
 
 function NewExpenseForm(){
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+    const[enteredAmount, setEnteredAmount] = useState('');
+
+    function titleChangeHandler(event){
+        setEnteredTitle(event.target.value);
+    }
+    function dateChangeHandler(event){
+        setEnteredDate(event.target.value);
+    }
+    function amountChangeHandler(event){
+        setEnteredAmount(event.target.value);
+    }
     return(
         <form>
             <div className="new-expense__controls ">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type='text'></input>
+                    <input type='text' onChange={titleChangeHandler}></input>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type='date'></input>
+                    <input type='date' min='2019-01-01' step='2025-12-31' onChange={dateChangeHandler}></input>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type='number'></input>
+                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}></input>
                 </div>
             </div>
             <div className="new-expense__actions">
