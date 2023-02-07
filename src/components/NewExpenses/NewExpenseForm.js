70bcf6a930b76448ba43704e03b3ad6db42e1043
Howard Mabhugu <hmabhugu.3@gmail.com>
2023-02-07 09:59:02 +0200

    two way bidding

--- src/components/NewExpenses/NewExpenseForm.js
+++ src/components/NewExpenses/NewExpenseForm.js
@@ -47,21 +47,24 @@ function NewExpenseForm(){
         }
 
         console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredDate('');
+        setEnteredAmount('');
     };
     return(
         <form onSubmit={submitHandler}>
             <div className="new-expense__controls ">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type='text' onChange={titleChangeHandler}></input>
+                    <input type='text' onChange={titleChangeHandler} value={enteredTitle} ></input>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type='date' min='2019-01-01' step='2025-12-31' onChange={dateChangeHandler}></input>
+                    <input type='date' min='2019-01-01' step='2025-12-31' onChange={dateChangeHandler} value={enteredDate} ></input>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}></input>
+                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} value={enteredAmount} ></input>
                 </div>
             </div>
             <div className="new-expense__actions">
