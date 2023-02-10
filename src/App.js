b8b6e671ee93d382e7de17e02d66bbcf16e0f3f1
Howard Mabhugu <hmabhugu.3@gmail.com>
2023-02-10 10:57:42 +0200

    added new expenses on display

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
+// import React, {useState} from 'react';
 import './App.css';
 import Expense from './components/Expenses/Expense';
 import Card from '../src/components/UI/Card';
 import NewExpense from './components/NewExpenses/New Expense';
+import { useState } from 'react';
 
 
 function App() {
-  const  expensesList =[
+  const  initialList =[
     {date: new Date(2023,1,31),title:'Toilet Paper', amount:12.90},
     {date: new Date(2023,1,31),title:'Insurance', amount:70.02},
     {date: new Date(2023,1,31),title:'Transport', amount:60.50},
     {date: new Date(2023,2,1),title:'Lunch', amount:30.50},
     {date: new Date(2023,2,2),title:'School Fees', amount:410},
   ];
+
+  const [expensesList,setExpensesList] = useState(initialList);
+
   function newExpenseHandler(receivedExpenses){
       // return(
       //   expensesList.put(receivedExpenses.date, receivedExpenses.title, receivedExpenses.amount)
       // );
-      console.log("In App.js");
-      console.log(expensesList);
-      console.log(receivedExpenses)
+
+      setExpensesList([receivedExpenses, ...initialList]);
   }
   return (
     <Card>
